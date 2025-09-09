15402eca46599506280c5f776b9689090eac4180
Afcgodoy <afcgodoy.1509@gmail.com>
2025-09-09 09:53:32 -0300

    Update download.py

--- Download Acordaos/download.py
+++ Download Acordaos/download.py
@@ -3,24 +3,30 @@ import time
 
 chrome_options = uc.ChromeOptions()
 
+#Definindo as configurações do driver
+
 chrome_options.add_experimental_option('prefs', {
-    "download.default_directory": r"C:\Users\AnaFláviaGodoy\Desktop\Acordaos\acordaos",
-    "download.directory_upgrade": True,
-    "download.prompt_for_download": False,
-    "plugins.always_open_pdf_externally": True,
+    "download.default_directory": r"C:\Users\AnaFláviaGodoy\Desktop\Acordaos\acordaos", #Definindo o diretório onde os arquivos devem ser baixados
+    "download.directory_upgrade": True, #Para atualizar o diretório onde os arquivos serão baixados
+    "download.prompt_for_download": False, #Para que não seja necessária uma ação do usuário para o início do download
+    "plugins.always_open_pdf_externally": True, #Para que o navegador não fique abrindo todos os PDFs que forem baixados
     "safebrowsing.enabled": True,
 })
 
+#Função para escrever no arquivo 'erros.txt' os links que tiveram erros no carregamento
 def listarErros(link, arquivo='erros.txt'):
     with open(arquivo, 'a') as arq:
         arq.write(f"{link}\n")
         
 navegador = uc.Chrome(options = chrome_options)
 
+#Para ler os links a serem consultados
 with open('links.txt', 'r') as arq:
     links = arq.read().splitlines()
 
 i=0
+
+#Loop para consultar todos os links
 for link in links:
     try:
         navegador.get(link)
