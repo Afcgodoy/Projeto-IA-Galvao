eae920afaec9ad114d9387c042d5ee807668fd23
Afcgodoy <afcgodoy.1509@gmail.com>
2025-09-09 09:43:35 -0300

    Update rodar.py

--- json/rodar.py
+++ json/rodar.py
@@ -2,12 +2,13 @@ import os
 import json
 from PyPDF2 import PdfReader
 
-# Nota: Certifique-se de instalar a biblioteca PyPDF2 com 'pip install PyPDF2' se não estiver instalada.
+#Pasta com os arquivos PDF a serem convertidos
+pasta = "arquivos"
 
-# Caminho da pasta com os PDFs
-pasta = "2024"
+#Nome do arquivo JSON onde quero que o conteúdo convertido seja inserido
+arquivo_json = 'cc2025.json'
 
-# Lista para armazenar os dados no formato pergunta-resposta
+# Lista para armazenar os dados no formato consulta-conteudo
 dados = []
 
 # Percorre todos os arquivos na pasta
@@ -22,18 +23,19 @@ for arquivo in os.listdir(pasta):
             with open(caminho_pdf, "rb") as f:
                 reader = PdfReader(f)
                 for page in reader.pages:
-                    texto += page.extract_text() + "\n"  # Adiciona uma nova linha entre páginas
+                    texto += page.extract_text()
         except Exception as e:
             texto = f"Erro ao extrair texto: {str(e)}"
         
-        # Adiciona ao dicionário
+        # Adicionar o conteúdo no formato desejado à lista/dicionário
         dados.append({
-            "pergunta": arquivo,
-            "resposta": texto.strip()  # Remove espaços extras no final
+            "consulta": arquivo,
+            "conteudo": texto.strip()
         })
 
 # Salva os dados em um arquivo JSON
-with open("cc2024.json", "w", encoding="utf-8") as json_file:
+with open(arquivo_json, "w", encoding="utf-8") as json_file:
     json.dump(dados, json_file, ensure_ascii=False, indent=4)
+
 
 print("Arquivo JSON criado com sucesso!")
